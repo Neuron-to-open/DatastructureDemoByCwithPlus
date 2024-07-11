543fc72367f2e57f3c4fe37df289d3af8ff77101
Neuron-to-open <apple.jiayy@icloud.com>
2024-07-11 10:50:12 +0800

    2024.07.11 -- 顺序栈的修改

--- SequenceStack.cpp
+++ SequenceStack.cpp
@@ -3,3 +3,60 @@
 //
 
 #include "SequenceStack.h"
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+
+SequenceStack::SequenceStack() {
+    InitSequenceStack();
+}
+
+void SequenceStack::InitSequenceStack() {
+    this->seqStack.top = -1;
+    this->seqStack.stack = (SequenceStackDataElement *) malloc(sizeof(SequenceStackDataElement) * MAX_STACK_SIZE);
+
+}
+
+bool SequenceStack::EmptySequenceStack() {
+    if (this->seqStack.top == -1) {
+        return true;
+    }
+    return false;
+}
+
+bool SequenceStack::Push(SequenceStackDataElement data) {
+    if (this->seqStack.top == MAX_STACK_SIZE - 1) {
+
+        cout << "Stack overflow!" << endl;
+        return false;
+    } else {
+        this->seqStack.top++;
+        this->seqStack.stack[this->seqStack.top] = data;
+        return true;
+    }
+}
+
+bool SequenceStack::Pop(SequenceStackDataElement &data) {
+    if (this->seqStack.top == -1) {
+        cout << "Stack is empty!" << endl;
+        return false;
+    } else {
+        data = this->seqStack.stack[this->seqStack.top];
+        this->seqStack.top--;
+        return true;
+    }
+}
+
+bool SequenceStack::GetTop(SequenceStackDataElement &data) {
+
+    if (this->seqStack.top == -1) {
+        cout << "Stack is empty!" << endl;
+        return false;
+    } else {
+        data = this->seqStack.stack[this->seqStack.top];
+        return true;
+    }
+
+}
+
+
--- SequenceStack.h
+++ SequenceStack.h
@@ -4,5 +4,36 @@
 
 #ifndef ALL_SEQUENCESTACK_H
 #define ALL_SEQUENCESTACK_H
+#define MAX_STACK_SIZE 100
 
+/* 定义元素类型为整型 */
+typedef int SequenceStackDataElement ;
+/* 定义顺序栈结构 */
+typedef struct {
+    SequenceStackDataElement *stack ;
+    int top ;
+}SeqStack ;
+
+/* 初始化顺序栈 */
+class SequenceStack {
+public:
+
+    SeqStack seqStack ; // 栈实例
+
+    SequenceStack();
+
+
+    void InitSequenceStack();
+
+    bool EmptySequenceStack();
+
+    bool Push(SequenceStackDataElement data) ;
+
+    bool Pop(SequenceStackDataElement &data) ;
+
+    bool GetTop(SequenceStackDataElement &data) ;
+
+
+
+};
 #endif //ALL_SEQUENCESTACK_H
