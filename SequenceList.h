e5591f5b9f621fb6aaba64bcbdfb66b3d74a5dd4
Neuron-to-open <apple.jiayy@icloud.com>
2024-07-11 17:44:14 +0800

    2024.07.11 -- 单链表注释的修改

--- SequenceList.h
+++ SequenceList.h
@@ -8,6 +8,19 @@
 /* 定义元素类型为整型 */
 typedef int SequenceListDataElement ;
 /* 定义顺序表结构 */
+/*
+ *
+ * typedef: 这个关键字用于为结构体定义一个新的类型名。在这里，我们使用typedef将结构体Node定义为一种新的类型，简化了以后的使用。
+
+struct Node: 这是结构体的定义。结构体是一种聚合数据类型，可以包含多个不同类型的数据。
+
+SingleListDataElement data: 这是结构体中的一个成员变量，名为data，它的类型是SingleListDataElement。这个变量用于存储节点的数据。SingleListDataElement可能是一个自定义的数据类型，在这段代码中没有具体定义，但可以是任意的数据类型，如整数、浮点数、字符、指针等。
+
+*struct Node next: 这是结构体中的另一个成员变量，名为next，它是一个指向结构体Node类型的指针。这个指针用于指向下一个节点，形成一个链表结构。
+
+Node: 在结构体定义的末尾，这个名字是通过typedef关键字引入的，表示我们可以使用Node来代替struct Node。这使得代码在创建和操作链表节点时更加简洁。
+
+总的来说，这段代码定义了一个链表节点的结构体，每个节点包含两个成员：一个用于存储数据的data和一个用于指向下一个节点的指针next。通过这种定义，可以方便地创建和操作单链表。*/
 typedef struct {
     SequenceListDataElement *head ; /* 指向数组首元素的指针 */
     int size ; /* 数组的预分配大小 */
